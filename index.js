6a413cb1b696758a646f09da8da06e16d0f80586
nathanmontt <contato.mont@gmail.com>
2024-12-24 00:10:57 -0300

    Adição de comentário explicativo

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
+'use strict'
+
+/* Por algum motivo, o JS só funciona com essa função de DOMContentLoaded. Como iniciante, não entendi ao certo do porquê não funciona sem, então deixarei aí */
 document.addEventListener("DOMContentLoaded", function () {
     const textClickMe = document.querySelector(".test")
 
